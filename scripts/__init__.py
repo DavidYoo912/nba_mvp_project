1654963419076bfb2f86561876ba3315f733fa20
dbtjdals <dy66323@gmail.com>
2022-01-10 17:20:27 -0500

    added scripts, changes to the notebook

---
+++ scripts/__init__.py
@@ -0,0 +1,16 @@
+"""
+This approach allows for clean relative imports when dealing with Juypter
+notebooks. All relative imports happen within this file and note in
+Jupyter notebooks.
+See: https://stackoverflow.com/questions/34478398/import-local-function-from-a-module-housed-in-another-directory-with-relative-im
+"""
+
+import os
+import sys
+
+module_path = os.path.dirname(os.getcwd()) + '/scripts'
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+   
+os.chdir("../..")
